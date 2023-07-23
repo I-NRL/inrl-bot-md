8e2576d2c598a7842574296cd7fedaf34b650eb1
Tomiwa Fadare <51441197+Razhfad@users.noreply.github.com>
2023-07-23 16:38:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,36 +2,36 @@ const toBool = (x) => x == 'true'
 const { existsSync } = require('fs')
 if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', //your ssid to run bot
-    MONGO_URL : process.env.MONGO_URL,//must be enter your mongo url;
+    SESSION_ID: process.env.SESSION_ID || 'inrl~16a563BNG0e8a8168a6268be157ad4a5832d', //your ssid to run bot
+    MONGO_URL : process.env.MONGO_URL,mongodb+srv://Razhfad:Random8900@cluster0.heis8kk.mongodb.net/?retryWrites=true&w=majority//must be enter your mongo url;
     HEROKU: {
         API_KEY: process.env.HEROKU_API_KEY,
         APP_NAME: process.env.HEROKU_APP_NAME
     },
     BASE_URL : "https://inrl-web.onrender.com/",
-    TUTORIAL : "\nreplit :- https://youtu.be/jwTQAQBH5mM\n\nheroku :- https://youtu.be/4WfgHllp1l8\n",
+    TUTORIAL : "\nreplit :- https://twitter.com/a_sad_nerd :- https://twitter.com/a_sad_nerd",
     PMB_MSG : "pm msgs isn't allowed",
     PMBC_MSG : "pm call isn't allowed",
     AUTOMUTE_MSG : "_group will been muted at @time_",
     AUTOUNMUTE_MSG : "_group will unmute at @time_",
-    GIT : "https://github.com/inrl-official/inrl-bot-md",
-    WAGRP : 'https://chat.whatsapp.com/ECjTvQMK5D6Bayzu6rJ7Ot',
-    ALLWAYS_ONLINE: toBool(process.env.ALLWAYS_ONLINE || "true"),
+    GIT : "https://github.com/Razhfad",
+    WAGRP : 'https://wa.me/2349093924119',
+    ALLWAYS_ONLINE: toBool(process.env.ALLWAYS_ONLINE || "false"),
     REACT : toBool(process.env.REACT || "false"),
-    ANTI_SPAM : toBool(process.env.ANTI_SPAM || "false"),
+    ANTI_SPAM : toBool(process.env.ANTI_SPAM || "true"),
     AUTO_BIO : toBool(process.env.AUTO_BIO || "false"),
     PM_BLOCK : toBool(process.env.PM_BLOCK || "false"),
     BGMBOT : toBool(process.env.BGMBOT || "false"),
     CALL_BLOCK : toBool(process.env.CALL_BLOCK || "false"),
-    STATUS_VIEW : toBool(process.env.STATUS_VIEW || "true"),
+    STATUS_VIEW : toBool(process.env.STATUS_VIEW || "false"),
     READ_CHAT : toBool(process.env.READ_CHAT ||  "false"),
     AUTO_CHAT_PM : toBool(process.env.AUTO_CHAT_PM || "false"),
     AUTO_CHAT_GRP : toBool(process.env.AUTO_CHAT_GRP || "false"),
-    FOOTER : process.env.FOOTER || "ɪɴʀʟ-ʙᴏᴛ-ᴍᴅ",
+    FOOTER : process.env.FOOTER || "Razhfad",
     PASSWORD : process.env.PASSWORD || 'inrl-bot~md',
     WARNCOUND : process.env.WARNCOUND || 5,
     ALIVE_DATA : process.env.ALIVE_DATA || "$text>_iam alive now &sender_",
-    BOT_INFO : process.env.BOT_INFO || "914040404010;INRL-BOT-MD;INRL;https://i.imgur.com/DyLAuEh.jpg",
+    BOT_INFO : process.env.BOT_INFO || "2349093924119;Night Hawk;Razhfad;https://imgur.com/a/4myCMKG.jpg",
     WORKTYPE : process.env.WORKTYPE || "private",
     PREFIX : process.env.PREFIX || false,
     WELCOME_MSG : process.env.WELCOME_MSG || "$text>_hey bro/sis_ *&user*\nthanks for join;$image>&pp;",
@@ -40,8 +40,8 @@ module.exports = {
     BLOCK_CHAT : process.env.BLOCK_CHAT || "jid@g.us, jid2@g.us",//set chat similarly
     BOT_PRESENCE : process.env.BOT_PRESENCE || "recording",
     AUDIO_DATA : process.env.AUDIO_DATA || "ᴍᴜꜱɪᴄ;ᴋɪᴅ;https://i.imgur.com/DyLAuEh.jpg",
-    STICKER_DATA : process.env.AUDIO_DATA || "inrl;inrl",
+    STICKER_DATA : process.env.AUDIO_DATA || "Pig Benis;2349093924119",
     INSTAGRAM :  process.env.INSTAGRAM || "nullX",
-    CAPTION : process.env.CAPTION || "_created by @inrl_",
-    SUDO : process.env.SUDO || "91707070701010"
+    CAPTION : process.env.CAPTION || "_created by @razhfad",
+    SUDO : process.env.SUDO || "2349093924119"
 };
