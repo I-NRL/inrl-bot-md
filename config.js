4435344cda0ffaad8a19573819d8820d3cc5f9fc
VIJAY <vijayayakumar06@gmail.com>
2023-07-28 15:31:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const toBool = (x) => x == 'true'
 const { existsSync } = require('fs')
 if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', //your ssid to run bot
+    SESSION_ID: process.env.SESSION_ID || 'inrl~fbd91NZzBcd705f4a523339ab7340d3657c6', //your ssid to run bot
     MONGO_URL : process.env.MONGO_URL,//must be enter your mongo url;
     HEROKU: {
         API_KEY: process.env.HEROKU_API_KEY,
@@ -17,17 +17,17 @@ module.exports = {
     ALLWAYS_ONLINE: toBool(process.env.ALLWAYS_ONLINE || "true"),
     REACT : toBool(process.env.REACT || "false"),
     ANTI_SPAM : toBool(process.env.ANTI_SPAM || "false"),
-    AUTO_BIO : toBool(process.env.AUTO_BIO || "false"),
+    AUTO_BIO : toBool(process.env.AUTO_BIO || "true"),
     PM_BLOCK : toBool(process.env.PM_BLOCK || "false"),
     BGMBOT : toBool(process.env.BGMBOT || "false"),
-    CALL_BLOCK : toBool(process.env.CALL_BLOCK || "false"),
+    CALL_BLOCK : toBool(process.env.CALL_BLOCK || "true"),
     STATUS_VIEW : toBool(process.env.STATUS_VIEW || "true"),
-    READ_CHAT : toBool(process.env.READ_CHAT ||  "false"),
+    READ_CHAT : toBool(process.env.READ_CHAT ||  "true"),
     AUTO_CHAT_PM : toBool(process.env.AUTO_CHAT_PM || "false"),
     AUTO_CHAT_GRP : toBool(process.env.AUTO_CHAT_GRP || "false"),
     WARNCOUND : process.env.WARNCOUND || 5,
     ALIVE_DATA : process.env.ALIVE_DATA || "$text>_iam alive now &sender_",
-    BOT_INFO : process.env.BOT_INFO || "INRL-BOT-MD;INRL;https://i.imgur.com/DyLAuEh.jpg",
+    BOT_INFO : process.env.BOT_INFO || "© ᴀᴅᴋ ʀᴉᴄⲏᴜ ᴘᴜʙᴌᴉᴄ ʙᴏᴛ;© ī.am/ᴀᴅᴋ_ʀᴉᴄʜᴜ;https://i.imgur.com/d7S55LY.jpeg",
     WORKTYPE : process.env.WORKTYPE || "private",
     PREFIX : process.env.PREFIX || false,
     WELCOME_MSG : process.env.WELCOME_MSG || "$text>_hey bro/sis_ *&user*\nthanks for join;$image>&pp;",
@@ -35,7 +35,19 @@ module.exports = {
     LANG : process.env.LANG || "en",
     BLOCK_CHAT : process.env.BLOCK_CHAT || "jid@g.us, jid2@g.us",//set chat similarly
     BOT_PRESENCE : process.env.BOT_PRESENCE || "recording",
-    AUDIO_DATA : process.env.AUDIO_DATA || "ᴍᴜꜱɪᴄ;ᴋɪᴅ;https://i.imgur.com/DyLAuEh.jpg",
-    STICKER_DATA : process.env.AUDIO_DATA || "inrl;inrl",
-    SUDO : process.env.SUDO || "91707070701010"
+    AUDIO_DATA : process.env.AUDIO_DATA || " ᴉᴛᴢ ᴍᴇ ᴀᴅᴋ_ʀᴉᴄʜᴜ;⇆        ◁ㅤㅤ ❚❚ㅤㅤ ▷        ↻                                                                                        0:09 ━━●────────── 0:19;https://i.imgur.com/d7S55LY.jpeg",
+    STICKER_DATA : process.env.AUDIO_DATA || "↢𝑖𝑡𝑠᪳ 𝑚𝑒͢⍣⃟🎀⏤͟͞⛧ꪶ𝐀𝐃𝐊᪳ 𝐑𝐈𝐂𝐇𝐔ꫂ⛧͟͞⏤
+
+
+
+𝑨 𝑴𝑶𝑫𝑼𝑳𝑨𝑹 𝑩𝑶𝑻 𝑴𝑨𝑫𝑬 𝑭𝑶𝑹 𝑾𝑯𝑨𝑻𝑺𝑨𝑷𝑷 💫
+
+
+
+🤌𝕷𝖀𝖁 𝖄𝕺𝖀 ❤‍🩹😙
+
+
+
+ +9162x2x22x03💖📱",
+    SUDO : process.env.SUDO || "916282122603"
 };
