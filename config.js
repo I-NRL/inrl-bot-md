be333ab1be72dd3c2992affe45fd440dc90c9ddb
bayuwe-yuzo <138072853+bayuwe-yuzo@users.noreply.github.com>
2023-10-10 02:01:26 +0300

    Update config.js
    Hello

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const { existsSync } = require('fs')
 if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 process.env.NODE_OPTIONS = '--max_old_space_size=2560'//2.5
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "", //your ssid to run bot
+    SESSION_ID: process.env.SESSION_ID || "inrl~1dc4brB3Qd2c4fa013d9aefa59c0faf85bd3" , //your ssid to run bot
     MONGO_URL : process.env.MONGO_URL||"",//must be enter your mongo url;
     HEROKU: {
         API_KEY: process.env.HEROKU_API_KEY,
