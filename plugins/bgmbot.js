e89a1fa55bac479da5e74844fc1c10e0a00a30e8
inrl-official <fasweehmon2@gmail.com>
2023-11-09 07:12:20 +0530

    init

--- plugins/bgmbot.js
+++ plugins/bgmbot.js
@@ -1,35 +1,31 @@
 const bgm = require('../media/bgm.json');
-const {inrl} = require('../lib');
-const {BGMBOT,PREFIX} = require('../config')
+const { inrl } = require('../lib');
+const { BGMBOT } = require('../config');
+
 inrl({
-	on: "text"
+        on: "text",
+        fromMe: 'public'
 }, async (m, match) => {
- if(m?.fromMe) return;
-if(m.isBot) return;
-if(PREFIX =='false') return;
- let audios=[], mp3, chk=[];
- let add = m.client.body.toLowerCase().trim();
- if(add.includes(' ')){
- add = add.split(' ');
-    add.map((p)=>{
-    chk.push(p)
-    })
-    } else {
-    chk.push(add)
-  }
- if(!BGMBOT) return;
- for (let key in bgm) {
- let a = [];a.push(key)
-  a.map((a)=>{
-  chk.forEach((c)=>{
-  if(c == a.toLowerCase()){
-  mp3 = bgm[key];
-  audios.push(mp3)
-  mp3 = audios[Math.floor(Math.random() * audios.length)];
-           }
-       })
-    })
-  }
- if(mp3 === undefined) return;
- return m.sock.sendMessage(m.from,{ audio: { url: mp3.trim() }, mimetype: "audio/mp4",ptt: true}, { quoted: m } );
+        if (!BGMBOT) return;
+        if (m.client.isCreator) return;
+        let audios = [];
+        const add = m.client.body.toLowerCase().trim().split(' ') || [m.client.body.toLowerCase().trim()];
+        for (let key in bgm) {
+                add.forEach(s => {
+                        if (s.toLowerCase() == key.toLowerCase()) {
+                                audios.push(bgm[key]);
+                        }
+                })
+        }
+        const mp3 = audios[Math.floor(Math.random() * audios.length)];
+        if (!mp3) return;
+        return await m.sock.sendMessage(m.from, {
+                audio: {
+                        url: mp3.trim()
+                },
+                mimetype: "audio/mp4",
+                ptt: true
+        }, {
+                quoted: m
+        });
 })
