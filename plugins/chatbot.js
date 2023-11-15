b06b2f55b72fb2bd148618c108cc697adbf30fd5
inrl-official <fasweehmon2@gmail.com>
2023-11-15 13:44:03 +0530

    Update chatbot.js

--- plugins/chatbot.js
+++ plugins/chatbot.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const {inrl} = require('../lib');
-const {CHATBOT_PM,CHATBOT_GRP,PREFIX} = require('../config');
+const {BRAINSHOP,CHATBOT_PM,CHATBOT_GRP,PREFIX} = require('../config');
 inrl({
 	on: "text"
 }, async (m, match) => {
@@ -8,10 +8,10 @@ inrl({
   if(m?.fromMe) return;
 if(PREFIX=='false') return;
   if(CHATBOT_PM && !m.isGroup){
-  let {data} = await axios(`http://api.brainshop.ai/get?bid=172372&key=nbjE0YAlyw3cpoMl&uid=[${m.sender}]&msg=[${m.client.body}]`)
+  let {data} = await axios(`http://api.brainshop.ai/get?bid=${BRAINSHOP.split(/[,;|]/)[0]}&key=${BRAINSHOP.split(/[,;|]/)[1]}&uid=[${m.sender.split('@')[0]}]&msg=[${m.client.body}]`)
   return await m.reply(data.cnt)
   } else if(CHATBOT_GRP && m.isGroup){
-  let {data} = await axios(`http://api.brainshop.ai/get?bid=172372&key=nbjE0YAlyw3cpoMl&uid=[${m.sender}]&msg=[${m.client.body}]`)
+  let {data} = await axios(`http://api.brainshop.ai/get?bid=${BRAINSHOP.split(/[,;|]/)[0]}&key=${BRAINSHOP.split(/[,;|]/)[1]}&uid=[${m.sender.split('@')[0]}]&msg=[${m.client.body}]`)
   return await m.reply(data.cnt)
   }
 });
