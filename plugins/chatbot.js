82c67f0cd45d0f4c82bd59f4e2d3d170dd009e14
inrl-official <fasweehmon2@gmail.com>
2024-01-01 09:42:28 +0530

    Fix

--- plugins/chatbot.js
+++ plugins/chatbot.js
@@ -11,12 +11,12 @@ inrl({
         if(m.isCreator) return;
         if(config.CHATBOT_PM && !m.isGroup) {
                 let data = await getJson(
-                        `http://api.config.BRAINSHOP.ai/get?bid=${config.BRAINSHOP.split(/[,;|]/)[0]}&key=${config.BRAINSHOP.split(/[,;|]/)[1]}&uid=[${m.sender.split('@')[0]}]&msg=[${m.body}]`
+                        `http://api.brainshop.ai/get?bid=${config.BRAINSHOP.split(/[,;|]/)[0]}&key=${config.BRAINSHOP.split(/[,;|]/)[1]}&uid=[${m.sender.split('@')[0]}]&msg=[${m.body}]`
                 )
                 return await m.reply(data.cnt)
         } else if(config.CHATBOT_GRP && m.isGroup) {
                 let data = await getJson(
-                        `http://api.config.BRAINSHOP.ai/get?bid=${config.BRAINSHOP.split(/[,;|]/)[0]}&key=${config.BRAINSHOP.split(/[,;|]/)[1]}&uid=[${m.sender.split('@')[0]}]&msg=[${m.body}]`
+                        `http://api.brainshop.ai/get?bid=${config.BRAINSHOP.split(/[,;|]/)[0]}&key=${config.BRAINSHOP.split(/[,;|]/)[1]}&uid=[${m.sender.split('@')[0]}]&msg=[${m.body}]`
                 )
                 return await m.reply(data.cnt)
         }
