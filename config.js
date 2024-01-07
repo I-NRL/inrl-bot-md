3e62fcf14443f70b8799ecdc04c782c76337cec1
Shazad233 <155872575+Shazad233@users.noreply.github.com>
2024-01-07 15:24:47 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 process.env.NODE_OPTIONS = '--max_old_space_size=2560'//2.5
 const DB_URL =  process.env.DATABASE_URL || '';
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', //your ssid to run bot
+    SESSION_ID: process.env.SESSION_ID || 'inrl~b3e07Vne4fd25f9050a13a8dc1bb5a5d6bbd', //your ssid to run bot
     HEROKU: {
         API_KEY: process.env.HEROKU_API_KEY,
         APP_NAME: process.env.HEROKU_APP_NAME
@@ -14,13 +14,13 @@ module.exports = {
     REPO: "inrl-official/inrl-bot-md",
     BGM_URL : process.env.BGM_URL || "null",
     REJECT_CALL : toBool(process.env.REJECT_CALL || 'false'),
-    BADWORD_BLOCK : toBool(process.env.BADWORD_BLOCK || 'false'),
-    ALLWAYS_ONLINE: toBool(process.env.ALLWAYS_ONLINE || "false"),
-    REACT : toBool(process.env.REACT || "false"),
+    BADWORD_BLOCK : toBool(process.env.BADWORD_BLOCK || 'true'),
+    ALLWAYS_ONLINE: toBool(process.env.ALLWAYS_ONLINE || "true"),
+    REACT : toBool(process.env.REACT || "true"),
     PM_BLOCK : toBool(process.env.PM_BLOCK || "false"),
-    BGMBOT : toBool(process.env.BGMBOT || "false"),
+    BGMBOT : toBool(process.env.BGMBOT || "true"),
     CALL_BLOCK : toBool(process.env.CALL_BLOCK || "false"),
-    REACT_CMD : toBool(process.env.REACT_CMD || "false"),
+    REACT_CMD : toBool(process.env.REACT_CMD || "true"),
     REACT_EMOJI : toBool(process.env.REACT_EMOJI || "false"),
     STATUS_VIEW : process.env.STATUS_VIEW || "false",
     SAVE_STATUS : toBool(process.env.SAVE_STATUS || "false"),
@@ -36,7 +36,7 @@ module.exports = {
     WARNCOUND : process.env.WARNCOUND || 5,
     ALIVE_DATA : process.env.ALIVE_DATA || "_iam alive now &sender_",
     BOT_INFO : process.env.BOT_INFO || "INRL-BOT-MD;INRL;https://i.imgur.com/DyLAuEh.jpg",
-    WORKTYPE : process.env.WORKTYPE || "private",
+    WORKTYPE : process.env.WORKTYPE || "Public",
     PREFIX : process.env.PREFIX || "[.,!]",
     LANG : process.env.LANG || "en",
     PERSONAL_MESSAGE: process.env.PERSONAL_MESSAGE || "null",
